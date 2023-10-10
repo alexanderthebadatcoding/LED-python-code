2b11cbcacf01b3d2cbf67a1d70cbd3b4f6868fab
Alexander Gilbertson <alexandertgilbertson@gmail.com>
2023-10-10 14:24:12 -0500

    Update espnAPI.py

--- espnAPI.py
+++ espnAPI.py
@@ -4,11 +4,11 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
 # Set up the RGB LED matrix
 options = RGBMatrixOptions()
-options.hardware_mapping = 'regular'  # Check your hardware mapping
+options.hardware_mapping = "regular"  # Check your hardware mapping
 matrix = RGBMatrix(options=options)
 
 # Define the ESPN API URL
-url = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/news?limit=50'
+url = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/news?limit=50"
 
 while True:
     # Make an API request to ESPN
@@ -17,10 +17,10 @@ while True:
         data = response.json()
         # Extract and display news on the LED matrix
         # Replace this part with your code to display news on the matrix
-        news_headlines = [article['headline'] for article in data.get('articles', [])]
+        news_headlines = [article["headline"] for article in data.get("articles", [])]
         for headline in news_headlines:
             matrix.Clear()
-            matrix.DrawText(0, 16, matrix.Color(255, 255, 255), headline)
+            matrix.draw.text(0, 16, matrix.Color(255, 255, 255), headline)
             time.sleep(5)  # Display each headline for 5 seconds
     else:
         print(f"Error: {response.status_code}")
