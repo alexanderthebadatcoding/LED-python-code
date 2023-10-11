0bd7eebc81b27fbaef316d77e719a88f8381557d
Alexander <127344545+alexanderthebadatcoding@users.noreply.github.com>
2023-10-11 10:43:41 -0500

    Update espn2.py
    added led-panel-type

--- espn2.py
+++ espn2.py
@@ -19,14 +19,26 @@ def fetch_nfl_headlines():
 
 # Command-line argument parsing
 parser = argparse.ArgumentParser()
-parser.add_argument("--led-rows", type=int, default=32, help="Number of rows in your RGB LED matrix")
-parser.add_argument("--led-cols", type=int, default=64, help="Number of columns in your RGB LED matrix")
-parser.add_argument("--led-chain", type=int, default=1, help="Number of daisy-chained panels")
-parser.add_argument("--led-parallel", type=int, default=1, help="Number of parallel chains")
-parser.add_argument("--led-pwm-bits", type=int, default=11, help="Bits used for PWM")
-parser.add_argument("--led-brightness", type=int, default=100, help="Brightness (0-100)")
-parser.add_argument("--led-parallel-map", type=str, default="default", help="Parallel pixel mapping")
-parser.add_argument("--led-gpio-mapping", type=str, default="regular", help="GPIO mapping")
+
+parser.add_argument("--led-rows", action="store", help="Display rows. 16 for 16x32, 32 for 32x32. (Default: 32)", default=32, type=int)
+parser.add_argument("--led-cols", action="store", help="Panel columns. Typically 32 or 64. (Default: 64)", default=64, type=int)
+parser.add_argument("--led-chain", action="store", help="Daisy-chained boards. (Default: 1)", default=1, type=int)
+parser.add_argument("--led-parallel", action="store", help="For Plus-models or RPi2: parallel chains. 1..3. (Default: 1)", default=1, type=int)
+parser.add_argument("--led-pwm-bits", action="store", help="Bits used for PWM. Range 1..11. (Default: 11)", default=11, type=int)
+parser.add_argument("--led-brightness", action="store", help="Sets brightness level. Range: 1..100. (Default: 100)", default=100, type=int)
+parser.add_argument("--led-gpio-mapping", help="Hardware Mapping: regular, adafruit-hat, adafruit-hat-pwm" , choices=['regular', 'adafruit-hat', 'adafruit-hat-pwm'], type=str)
+parser.add_argument("--led-scan-mode", action="store", help="Progressive or interlaced scan. 0 = Progressive, 1 = Interlaced. (Default: 1)", default=1, choices=range(2), type=int)
+parser.add_argument("--led-pwm-lsb-nanoseconds", action="store", help="Base time-unit for the on-time in the lowest significant bit in nanoseconds. (Default: 130)", default=130, type=int)
+parser.add_argument("--led-show-refresh", action="store_true", help="Shows the current refresh rate of the LED panel.")
+parser.add_argument("--led-slowdown-gpio", action="store", help="Slow down writing to GPIO. Range: 0..4. (Default: 1)", choices=range(5), type=int)
+parser.add_argument("--led-no-hardware-pulse", action="store", help="Don't use hardware pin-pulse generation.")
+parser.add_argument("--led-rgb-sequence", action="store", help="Switch if your matrix has led colors swapped. (Default: RGB)", default="RGB", type=str)
+parser.add_argument("--led-pixel-mapper", action="store", help="Apply pixel mappers. e.g \"Rotate:90\"", default="", type=str)
+parser.add_argument("--led-row-addr-type", action="store", help="0 = default; 1 = AB-addressed panels. (Default: 0)", default=0, type=int, choices=[0,1])
+parser.add_argument("--led-multiplexing", action="store", help="Multiplexing type: 0 = direct; 1 = strip; 2 = checker; 3 = spiral; 4 = Z-strip; 5 = ZnMirrorZStripe; 6 = coreman; 7 = Kaler2Scan; 8 = ZStripeUneven. (Default: 0)", default=0, type=int)
+parser.add_argument("--led-panel-type", action="store", help="Chipset of the panel. In particular if it doesn't light up at all, you might need to play with this option because it indicates that the panel requires a particular initialization sequence.", default=0, type=str)
+
+
 args = parser.parse_args()
 
 # Create an RGBMatrix instance
